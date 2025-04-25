04792142d0ba5b52aaf52532b38cc591767a5a94
protodeleter <38853864+protodeleter@users.noreply.github.com>
2025-04-25 10:24:40 +0300

    comm

--- actions.js
+++ actions.js
@@ -1,21 +1,48 @@
 'use_strict';
+const actions = () => {
+  const close_button = document.getElementById('close-bttn');
+  const add_new = document.getElementById('add-new');
+  const edit = document.getElementById('edit');
+  const info = document.getElementById('info');
 
-const close_button = document.getElementById('close-bttn');
-const add_new = document.getElementById('add-new');
-const edit = document.getElementById('edit');
-const info = document.getElementById('info');
-
-
-
-close_button.addEventListener('click', function (e) {
+  close_button.addEventListener('click', e => {
     close_popup();
-})
+  });
 
-
-add_new.addEventListener('click', function (e) {
+  add_new.addEventListener('click', e => {
     e.preventDefault();
 
-    console.log('testest');
+    add_item([
+      {
+        id: 1,
+        name: 'John Doe',
+        phone: '235235',
+        address: '123 Main St',
+        age: 25,
+        image_url: 'https://example.com/image1.jpg',
+      },
+      {
+        id: 1,
+        name: 'Johnasfasf',
+        phone: '235235',
+        address: '123 Main St',
+        age: 25,
+        image_url: 'https://example.com/image1.jpg',
+      },
+    ]);
 
     open_popup();
-})
+  });
+
+  edit.addEventListener('click', e => {
+    e.preventDefault();
+    open_popup();
+  });
+
+  info.addEventListener('click', e => {
+    e.preventDefault();
+    open_popup();
+  });
+};
+
+actions();
--- index.js
+++ index.js
@@ -1,56 +1,59 @@
 'use_strict';
 
 let db = {
-    users: [
-        {
-            id: 1, name: 'John Doe', phone: '235235', address: '123 Main St', age: 25, image_url: 'https://example.com/image1.jpg',
-        },
+  users: [
+    {
+      id: 1,
+      name: 'John Doe',
+      phone: '235235',
+      address: '123 Main St',
+      age: 25,
+      image_url: 'https://example.com/image1.jpg',
+    },
 
-        {
-            id: 1, name: 'John Doe', phone: '235235', address: '123 Main St', age: 25, image_url: 'https://example.com/image1.jpg',
-        }
-    ]
+    {
+      id: 1,
+      name: 'John Doe',
+      phone: '235235',
+      address: '123 Main St',
+      age: 25,
+      image_url: 'https://example.com/image1.jpg',
+    },
+  ],
 };
 
-
-
 const getUsers = () => {
-
-
-    const usersNew = db.users.map((user) => {
-        return {
-            id: user.id,
-            name: user.name,
-            phone: user.phone,
-            address: user.address,
-            age: user.age,
-            image_url: user.image_url
-        };
-    })
-
-    return usersNew;
-}
-
-
-
-
-const getUserById = (id) => {
-    const user = db.users.find((user) => user.id === id);
-    if (!user) {
-        return null;
-    }
+  const usersNew = db.users.map(user => {
     return {
-        id: user.id,
-        name: user.name,
-        phone: user.phone,
-        address: user.address,
-        age: user.age,
-        image_url: user.image_url
+      id: user.id,
+      name: user.name,
+      phone: user.phone,
+      address: user.address,
+      age: user.age,
+      image_url: user.image_url,
     };
-}
+  });
 
-const user_item_template = (user) => {
-    return `
+  return usersNew;
+};
+
+const getUserById = id => {
+  const user = db.users.find(user => user.id === id);
+  if (!user) {
+    return null;
+  }
+  return {
+    id: user.id,
+    name: user.name,
+    phone: user.phone,
+    address: user.address,
+    age: user.age,
+    image_url: user.image_url,
+  };
+};
+
+const user_item_template = user => {
+  return `
         <div class="item general-actions flex f-j-sb">
 
             <div class="left flex">
@@ -64,54 +67,39 @@ const user_item_template = (user) => {
 
             <div class="right flex">
                 <div class="buttons">
-                    <a href="#" class="edit"> edit </a>
-                    <a href="#" class="del"> del </a>
-                    <a href="#" class="del"> info </a>
+                    <a href="#" class="edit" id="edit"> edit </a>
+                    <a href="#" class="del" id="del"> del </a>
+                    <a href="#" class="info" id="info"> info </a>
                 </div>
             </div>
 
         </div>
     `;
-}
-const render_users = (users) => {
+};
+const render_users = (users, callback) => {
+  const container = document.getElementById('user-list');
+  container.innerHTML = ''; // Clear previous content
 
-    console.log(users);
-
-
-    const container = document.getElementById('user-list');
-    container.innerHTML = ''; // Clear previous content
-
-
-    users.forEach((user) => {
-
-
-
-        const userHTML = user_item_template(user);
-        container.innerHTML += userHTML;
-    });
-}
+  users.forEach(user => {
+    const userHTML = user_item_template(user);
+    container.innerHTML += userHTML;
+  });
+};
 
 render_users(getUsers());
 
-const add_item = () => {
+const add_item = data => {
+  let asfasf = db.users.push(data);
 
-    const add_button = document.getElementById('add-new');
-
-
-}
+  render_users(data);
+};
 
 const close_popup = () => {
-    const popup = document.getElementById('popup');
-    popup.style.display = 'none';
-}
+  const popup = document.getElementById('popup');
+  popup.style.display = 'none';
+};
 
 const open_popup = () => {
-    const popup = document.getElementById('popup');
-    console.log('asfasf');
-
-    popup.style.display = 'block';
-}
-
-
-
-
+  const popup = document.getElementById('popup');
+  popup.style.display = 'block';
+};
